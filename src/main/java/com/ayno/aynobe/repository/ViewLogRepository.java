38af96d3d4b7c0b6e90991a89c465f10f70f9fc0
yangjunsig <wnstlr1379@naver.com>
2025-12-02 20:26:50 +0900

    feature: 필요없는 유저 뷰로그 api 제거

--- src/main/java/com/ayno/aynobe/repository/ViewLogRepository.java
+++ src/main/java/com/ayno/aynobe/repository/ViewLogRepository.java
@@ -1,21 +1,13 @@
 package com.ayno.aynobe.repository;
 
 import com.ayno.aynobe.entity.log.ViewLog;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface ViewLogRepository extends JpaRepository<ViewLog, Long> {
-
     Optional<ViewLog> findByArtifact_ArtifactIdAndUser_UserIdAndVisitDate(
             Long artifactId, Long userId, LocalDate visitDate
-    );
-
-    Page<ViewLog> findByUser_UserIdAndEnteredAtBetweenOrderByEnteredAtDesc(
-            Long userId, LocalDateTime from, LocalDateTime to, Pageable pageable
     );
 }
